6942a2bac2e795d7c5b0fc1f8f0f2f4286af87cc
DOL-DOM <haesolj@gmail.com>
2024-12-09 23:43:01 +0900

    패널 사라지고 체력바 뜬 뒤 공이 안날아옴

--- Assets/scripts/LaserPointerHandler.cs
+++ Assets/scripts/LaserPointerHandler.cs
@@ -6,6 +6,8 @@ public class LaserPointerHandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer; // 레이저 포인터
     public SteamVR_Action_Boolean interactUIAction; // InteractUI 액션
+     public GameObject startMainCanvas; // StartMainCanvas 오브젝트
+    public GameManager gameManager; // GameManager 스크립트 참조
 
     void Awake()
     {
@@ -22,13 +24,35 @@ public class LaserPointerHandler : MonoBehaviour
     private void OnPointerClick(object sender, PointerEventArgs e)
     {
         // 선택된 오브젝트가 캡슐인지 확인
-        if (e.target.CompareTag("Capsule"))
-        {
+         if (e.target.CompareTag("Capsule"))
+         {
             // 캡슐의 색상 변경
-            Renderer renderer = e.target.GetComponent<Renderer>();
-            if (renderer != null)
+             Renderer renderer = e.target.GetComponent<Renderer>();
+             if (renderer != null)
+             {
+                 renderer.material.color = Color.red; // 색상을 빨간색으로 변경
+                 
+             }
+           
+            //gameManager.StartPanelInput();
+            // GameManager의 StartPanelInput 호출
+            
+            GameManager.instance.StartPanelInput();
+             // 캔버스 비활성화
+            startMainCanvas.SetActive(false);     
+            
+        };
+         
+        // 선택된 오브젝트가 StartMainCanvas인지 확인
+        if (e.target.CompareTag("Canvas"))
+        {
+            // 캔버스 비활성화
+            startMainCanvas.SetActive(false);
+
+            // GameManager의 StartPanelInput 호출
+            if (gameManager != null)
             {
-                renderer.material.color = Color.red; // 색상을 빨간색으로 변경
+                gameManager.StartPanelInput();
             }
         }
     }
